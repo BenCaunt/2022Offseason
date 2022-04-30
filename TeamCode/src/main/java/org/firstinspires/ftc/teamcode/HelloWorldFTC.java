ca43c9daa3c825a3d1916e65bca4d215b917ef3d
Ben Caunt <bdcaunt@gmail.com>
2022-04-30 17:25:05 -0400

    Hello world for FTC

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldFTC.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class HelloWorldFTC extends LinearOpMode {
+	@Override
+	public void runOpMode() throws InterruptedException {
+
+		ElapsedTime timer = new ElapsedTime();
+		DcMotorEx testMotor = hardwareMap.get(DcMotorEx.class, "NameOnDriverStation");
+		// everything before this runs during init
+		waitForStart();
+		timer.reset();
+		while (opModeIsActive()) {
+			System.out.println("Delta time" + timer.seconds());
+			testMotor.setPower(1);
+			timer.reset();
+		}
+	}
+}
