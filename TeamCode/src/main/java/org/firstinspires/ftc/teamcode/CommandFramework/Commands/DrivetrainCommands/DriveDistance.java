5a914ee435a59474e4e6b2010bbeb14f286a3294
Ben Caunt <bdcaunt@gmail.com>
2022-04-30 23:09:22 -0400

    Implement Nonlinear feedback control using lyapunov function [citation needed]

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandFramework/Commands/DrivetrainCommands/DriveDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandFramework/Commands/DrivetrainCommands/DriveDistance.java
@@ -1,4 +1,69 @@
 package org.firstinspires.ftc.teamcode.CommandFramework.Commands.DrivetrainCommands;
 
-public class DriveDistance {
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.ThermalEquilibrium.homeostasis.Utils.Vector;
+
+import org.firstinspires.ftc.teamcode.CommandFramework.Commands.Command;
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystems.Odometry;
+import org.firstinspires.ftc.teamcode.Math.Controllers.DistanceDriveControl;
+import org.firstinspires.ftc.teamcode.Utils.ExtraUtils;
+
+import java.util.function.DoubleSupplier;
+
+public class DriveDistance extends Command {
+
+	DistanceDriveControl control;
+	Drivetrain drivetrain;
+	Odometry odom;
+
+	double targetDistance;
+
+	Vector initialPosition;
+	private boolean isComplete = false;
+
+	public DriveDistance(Drivetrain drivetrain, Odometry odom, double targetDistance) {
+		this.drivetrain = drivetrain;
+		this.odom = odom;
+		this.targetDistance = targetDistance;
+	}
+
+
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	@Override
+	public void init() {
+		initialPosition = odom.getPosition();
+		double initialAngle = odom.getPosition().get(2);
+
+		control = new DistanceDriveControl(new DoubleSupplier() {
+			@Override
+			public double getAsDouble() {
+				return odom.getPosition().get(2);
+			}
+		}, initialAngle);
+
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	@Override
+	public void periodic() {
+		double traveled = ExtraUtils.calculateDistance(initialPosition,
+				odom.getPosition());
+		drivetrain.setPower(control.calculate(targetDistance,traveled));
+		isComplete = Math.abs(control.endPoseError()) < 1 && Math.abs(odom.getVelocity().get(0)) < 3;
+
+	}
+
+	@Override
+	public boolean stop() {
+		return isComplete;
+	}
+
+	@Override
+	public void shutdown() {
+		drivetrain.setPower(0,0);
+	}
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandFramework/Commands/DrivetrainCommands/TurnCommand.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.CommandFramework.Commands.DrivetrainCommands;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import org.firstinspires.ftc.teamcode.CommandFramework.Commands.Command;
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystems.Odometry;
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystems.Robot;
+import org.firstinspires.ftc.teamcode.Math.Controllers.TurnOnlyControl;
+
+import java.util.function.DoubleSupplier;
+
+public class TurnCommand extends Command {
+
+	Drivetrain drivetrain;
+	boolean isComplete = false;
+	Odometry odometry;
+
+	double referenceAngle;
+	TurnOnlyControl controller;
+
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	public TurnCommand(Drivetrain drivetrain, Odometry odometry, double referenceAngle) {
+		this.drivetrain = drivetrain;
+		this.referenceAngle = referenceAngle;
+		this.controller = new TurnOnlyControl(new DoubleSupplier() {
+			@Override
+			public double getAsDouble() {
+				return odometry.getPosition().get(2);
+			}
+		},referenceAngle);
+	}
+
+	@Override
+	public void init() {
+
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	@Override
+	public void periodic() {
+		drivetrain.setPower(controller.calculate());
+		isComplete = Math.abs(controller.getEndGoalError()) < Math.toRadians(2)
+				&& Math.abs(odometry.getVelocity().get(2)) < Math.toRadians(10);
+	}
+
+	@Override
+	public boolean stop() {
+		return isComplete;
+	}
+
+	@Override
+	public void shutdown() {
+		drivetrain.setPower(0,0);
+	}
+}
